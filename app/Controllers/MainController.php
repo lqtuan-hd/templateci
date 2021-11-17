994688b14f38d6a422efd5d55802d9e4e9373bc1
Your Name <you@example.com>
2021-11-17 11:46:58 +0700

    a

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,9 @@
+<?php namespace App\Controllers;
+
+class MainController extends BaseController
+{
+	public function index()
+	{
+		
+	}
+}
---
+++ app/Controllers/RegisterController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Controllers;
+
+class RegisterController extends BaseController
+{
+	public function index()
+	{
+
+		return view('auth-login');
+	}
+}
